1a476e8947ec9bcc1767157a150fb5898c946201
zhongjie-cai <zhongjie.cai@gmail.com>
2022-02-11 21:35:55 +0000

    Example using MemContextProvider.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System;
+using System.IO;
 
 namespace ConsoleApp
 {
@@ -14,14 +15,17 @@ namespace ConsoleApp
 
         public static void Main(string[] args)
         {
-            var config = CreateDummyConfiguration(args);
-            var manager = Init(config);
+            var serializer = new JsonObjectSerializer();
+            var context = new MemContextProvider(serializer);
+            var config = CreateDummyConfiguration(args, context);
+            var manager = Init(config, context, serializer);
             var result = manager.Get();
             var content = JsonConvert.SerializeObject(result, Formatting.Indented);
             Console.WriteLine(content);
+            Finalize(args, context);
         }
 
-        private static IConfiguration CreateDummyConfiguration(string[] args)
+        private static IConfiguration CreateDummyConfiguration(string[] args, MemContextProvider context)
         {
             var dummyConfiguration = new DummyConfiguration();
             if (args.Length == 1)
@@ -29,10 +33,12 @@ namespace ConsoleApp
                 if (args[0] == "r")
                 {
                     dummyConfiguration["dcir"] = "test_data";
+                    context.StartRecording("test_data");
                 }
                 else if (args[0] == "p")
                 {
                     dummyConfiguration["dcip"] = "test_data";
+                    context.StartReplaying("test_data", new StreamReader("test_data"));
                 }
             }
             else if (args.Length == 2)
@@ -40,24 +46,52 @@ namespace ConsoleApp
                 if (args[0] == "r")
                 {
                     dummyConfiguration["dcir"] = args[1];
+                    context.StartRecording(args[1]);
                 }
                 else if (args[0] == "p")
                 {
                     dummyConfiguration["dcip"] = args[1];
+                    context.StartReplaying(args[1], new StreamReader(args[1]));
                 }
             }
             return dummyConfiguration;
         }
 
-        private static IExampleManager Init(IConfiguration configuration)
+        private static IExampleManager Init(IConfiguration configuration, IProvideContext context, ISerializeObject serializer)
         {
-            InterceptorConfiguration.ConfigureFor<FileContextProvider, JsonObjectSerializer>(_container);
+            InterceptorConfiguration.ConfigureFor(_container, context, serializer);
             _container.Register(Component.For<IRandomNumber>().ImplementedBy<RandomNumber>().LifestyleSingleton());
             _container.Register(Component.For<IRandomActor>().ImplementedBy<RandomActor>().LifestyleSingleton());
             _container.Register(Component.For<IExampleManager>().ImplementedBy<ExampleManager>().LifestyleSingleton());
             _container.Register(Component.For<ILogger>().ImplementedBy<DummyLogger>().LifestyleSingleton());
             _container.Register(Component.For<IConfiguration>().Instance(configuration));
             return _container.Resolve<IExampleManager>();
+        }
+
+        private static void Finalize(string[] args, MemContextProvider context)
+        {
+            if (args.Length == 1)
+            {
+                if (args[0] == "r")
+                {
+                    context.StopRecording("test_data", new StreamWriter("test_data"));
+                }
+                else if (args[0] == "p")
+                {
+                    context.StopReplaying("test_data");
+                }
+            }
+            else if (args.Length == 2)
+            {
+                if (args[0] == "r")
+                {
+                    context.StopRecording(args[1], new StreamWriter(args[1]));
+                }
+                else if (args[0] == "p")
+                {
+                    context.StopReplaying(args[1]);
+                }
+            }
         }
     }
 }
